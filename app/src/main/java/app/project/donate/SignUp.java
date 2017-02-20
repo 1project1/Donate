b5e0ec38e02c05984879531b210eb8d82f61d6f8
aakash-a <123aakashsharma@gmail.com>
2017-02-20 21:36:22 +0530

    Inital Commit

---
+++ app/src/main/java/app/project/donate/SignUp.java
@@ -0,0 +1,114 @@
+package app.project.donate;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+/**
+ * Created by Aakash on 20-Feb-17.
+ */
+
+public class SignUp extends AppCompatActivity implements View.OnClickListener {
+    Button signUp;
+    EditText userName, password, confirmPassword;
+    private FirebaseAuth mAuth;
+
+    private final String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sign_up);
+
+        init();
+
+        signUp.setOnClickListener(this);
+
+
+        userName.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (s.equals(null) || s.length() == 0 || s.equals(""))
+                    userName.setError("Required");
+                else {
+                    userName.setError(null);
+                }
+            }
+        });
+    }
+
+    private void init() {
+        // FireBase
+        mAuth = FirebaseAuth.getInstance();
+
+        userName = (EditText) findViewById(R.id.email_signup);
+        password = (EditText) findViewById(R.id.password_signup);
+        confirmPassword = (EditText) findViewById(R.id.confirm_password_signup);
+
+        signUp = (Button) findViewById(R.id.signUp);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.signUp:
+                String u = userName.getText().toString();
+                String p = password.getText().toString();
+                String cp = confirmPassword.getText().toString();
+                if (u.isEmpty() || p.isEmpty() ||cp.isEmpty()) {
+                    Snackbar.make(v, "Fill All details", Snackbar.LENGTH_LONG).show();
+                    return;
+                }
+                if(!p.equals(cp)) {
+                    password.setError("Passowords don't Match");
+                    confirmPassword.setError("Passowords don't Match");
+                }
+
+                mAuth.createUserWithEmailAndPassword(u, p)
+                        .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                Log.d("Firebase", "createUserWithEmail:onComplete:" + task.isSuccessful());
+
+                                // If sign in fails, display a message to the user. If sign in succeeds
+                                // the auth state listener will be notified and logic to handle the
+                                // signed in user can be handled in the listener.
+                                if (!task.isSuccessful()) {
+                                    Toast.makeText(getApplicationContext(), "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+                                }
+
+                                // ...
+                            }
+                        });
+
+                break;
+
+        }
+    }
+}
+
---
+++ app/src/main/java/app/project/donate/Temp.java
@@ -0,0 +1,115 @@
+package app.project.donate;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.firebase.auth.FirebaseAuth;
+
+/**
+ * Created by Aakash on 20-Feb-17.
+ */
+
+public class Temp extends AppCompatActivity {
+    private GoogleApiClient mGoogleApiClient;
+
+    TextView tempDisplay;
+    public static final String LOGIN_FILE = "LogInFile";
+    SharedPreferences logInPref;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_temp);
+
+        init();
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this /* FragmentActivity */, null /* OnConnectionFailedListener */)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+        Log.i("password", logInPref.getString("user", "a user Name Here"));
+
+        String temp = logInPref.getString("user", "a user Name Here");
+        temp += "\n" + logInPref.getString("password", "a password here is displayed");
+
+        tempDisplay.setText(temp);
+
+        Button b = (Button) findViewById(R.id.log_out);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                SharedPreferences logInPref = getSharedPreferences(LOGIN_FILE, 0);
+                SharedPreferences.Editor logInEditor = logInPref.edit();
+                logInEditor.clear().putBoolean("isLoggedIn", false).apply();
+
+                Auth.GoogleSignInApi.signOut(mGoogleApiClient).setResultCallback(
+                        new ResultCallback<Status>() {
+                            @Override
+                            public void onResult(Status status) {
+                                // [START_EXCLUDE]
+                                // aakash updateUI(false);
+                                // [END_EXCLUDE]
+                            }
+                        });
+
+                startActivity(new Intent(getApplicationContext(), LogIn.class)
+                        .setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
+                finish();
+            }
+        });
+    }
+
+    private void init() {
+        tempDisplay = (TextView) findViewById(R.id.tempDisplay);
+        logInPref = getSharedPreferences(LOGIN_FILE, 0);
+    }
+
+
+    public void Bu1(View view) {
+        final ArrayAdapter<String> languages = new ArrayAdapter<String>(view.getContext(), android.R.layout.select_dialog_item, getResources().getStringArray(R.array.languages));
+
+        AlertDialog.Builder ad = new AlertDialog.Builder(this).setTitle(getResources().getString(R.string.select_language))
+                .setNegativeButton(getResources().getString(R.string.cancel), null);
+        ad.setAdapter(languages, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String selectedLang = languages.getItem(which);
+                changeLanguage(selectedLang);
+            }
+        }).show();
+
+
+    }
+
+    private void changeLanguage(String lang) {
+        Log.i("Lang",lang);
+        if (lang.equals("Hindi")) {
+            LocaleHelper.setLocale(this, "hi");
+        } else {
+            Context context = LocaleHelper.setLocale(this, "en");
+        }
+    }
+}
+
