c5769ee5d502c639c8205779ec3dfb4df59e9ef2
androidDev1617 <amanschauhan07@gmail.com>
2017-03-02 19:32:46 +0530

    Updated Main UI

---
+++ app/src/main/java/app/project/donate/utils/GetDonationCardDetails.java
@@ -0,0 +1,16 @@
+package app.project.donate.utils;
+
+/**
+ * Created by AmanPC on 02-03-2017.
+ */
+
+public class GetDonationCardDetails {
+    private String title, quantity;
+
+    public GetDonationCardDetails(){}
+
+    public GetDonationCardDetails(String title, String quantity){
+        this.title = title;
+        this.quantity = quantity;
+    }
+}
