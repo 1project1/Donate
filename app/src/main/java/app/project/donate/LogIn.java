9cd9502e2c87fc27ae0950f6f92beb56f92dd165
mruProjectTT <arup.professional@gmail.com>
2017-03-04 13:11:54 +0530

    ok

--- app/src/main/java/app/project/donate/LogIn.java
+++ app/src/main/java/app/project/donate/LogIn.java
@@ -1,18 +1,15 @@
 package app.project.donate;
 
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Typeface;
-import android.graphics.drawable.Drawable;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -29,7 +26,6 @@ import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.FirebaseException;
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
@@ -82,7 +78,7 @@ public class LogIn extends DialogActivity implements View.OnClickListener, Googl
                     logInEditor.putString("name", user.getDisplayName());
                     logInEditor.putBoolean("isLoggedIn", true);
                     logInEditor.apply();
-                    Intent i = new Intent(getApplicationContext(), Cart.class).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    Intent i = new Intent(getApplicationContext(), Temp.class).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
                     finish();
                     startActivity(i);
 
@@ -232,6 +228,9 @@ public class LogIn extends DialogActivity implements View.OnClickListener, Googl
                 firebaseAuthWithGoogle(account);
             } else {
                 Log.i(TAG, "Failed SignIn");
+                Toast.makeText(getApplicationContext(), "Authentication failed.",
+                        Toast.LENGTH_SHORT).show();
+
                 // Google Sign In failed, update UI appropriately
                 // [START_EXCLUDE]
                 /// aakash updateUI(null);
