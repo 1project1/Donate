7f6541925c91b64913a65c8566354dc72599d454
hkukreti <painfullboy@gmail.com>
2017-03-14 21:17:29 +0530

    yooo

--- app/src/main/java/app/project/donate/MainUi.java
+++ app/src/main/java/app/project/donate/MainUi.java
@@ -1,8 +1,8 @@
 package app.project.donate;
-
-import android.content.Context;
+ 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,59 +12,52 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ExpandableListView;
-
+ 
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.common.api.Status;
 import com.google.firebase.auth.FirebaseAuth;
-
+ 
 import app.project.donate.model.NgoList;
-
-public class MainUi extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+ 
+public class MainUi extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private GoogleApiClient mGoogleApiClient;
     public static final String LOGIN_FILE = "LogInFile";
-
-    final Context context = this;
-    private static final String data[][] = {{"CLOTHES"},{"BOOKS"},{"FOOD ITEMS"},{"TOYS"},{"UTENSILS"}};
-    private ExpandableListView expandableListView;
-
+ 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_ui);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-
-
+ 
+ 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
-
+ 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
         init();
     }
-
+ 
     private void init() {
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestIdToken(getString(R.string.default_web_client_id))
                 .requestEmail()
                 .build();
-
+ 
         mGoogleApiClient = new GoogleApiClient.Builder(this)
                 .enableAutoManage(this /* FragmentActivity */, null /* OnConnectionFailedListener */)
                 .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
                 .build();
-
+ 
     }
-
+ 
     @Override
     public void onBackPressed() {
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -74,34 +67,35 @@ public class MainUi extends AppCompatActivity
             super.onBackPressed();
         }
     }
-
+ 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main_ui, menu);
         return true;
     }
-
+ 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
+ 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            startActivity(new Intent(this,AccountSettings.class));
             return true;
         }
         else if(id == R.id.action_logout){
-
+ 
             SharedPreferences logInPref = getSharedPreferences(LOGIN_FILE, 0);
             SharedPreferences.Editor logInEditor = logInPref.edit();
             logInEditor.clear().putBoolean("isLoggedIn", false).apply();
-
+ 
             //firebase signOut
             FirebaseAuth.getInstance().signOut();
-
+ 
             //Google signOut
             Auth.GoogleSignInApi.signOut(mGoogleApiClient).setResultCallback(
                     new ResultCallback<Status>() {
@@ -112,22 +106,22 @@ public class MainUi extends AppCompatActivity
                             // [END_EXCLUDE]
                         }
                     });
-
+ 
             startActivity(new Intent(getApplicationContext(), LogIn.class)
                     .setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
             finish();
-
+ 
         }
-
+ 
         return super.onOptionsItemSelected(item);
     }
-
+ 
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
-
+ 
         if (id == R.id.nav_about) {
             // Handle the camera action
         } else if (id == R.id.nav_cart) {
@@ -137,15 +131,19 @@ public class MainUi extends AppCompatActivity
         } else if (id == R.id.nav_ngo_list) {
             startActivity(new Intent(this, NgoList.class));
         } else if (id == R.id.nav_rate_us) {
-
+            Uri uriUrl = Uri.parse("https://docs.google.com/forms/d/1yln_gJBWt7N-Mrk0z47MB-TIpRZ3PgOTE4H2iYblSGo/viewform?edit_requested=true");
+            Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
+            startActivity(launchBrowser);
         } else if (id == R.id.nav_credits) {
-
+ 
+            startActivity(new Intent(this, CreditsUI.class));
+ 
         }
-
+ 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
-
-
+ 
+ 
 }
