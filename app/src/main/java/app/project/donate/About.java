0001b44124da8bd819474fc51082938a9f2c1d1e
androidDev1617 <amanschauhan07@gmail.com>
2017-03-18 18:54:22 +0530

    Size of app reduced with new Logo

--- app/src/main/java/app/project/donate/About.java
+++ app/src/main/java/app/project/donate/About.java
@@ -16,7 +16,7 @@ public class About extends AppCompatActivity{
         TextView textView1 = (TextView) findViewById(R.id.abouttextView1);
         TextView textView2 = (TextView) findViewById(R.id.abouttextView2);
 
-        getSupportActionBar().setTitle("About JeeOne");
+        getSupportActionBar().setTitle("JeeOne");
 
         Typeface typeface = Typeface.createFromAsset(getAssets(),"fonts/billabong.ttf");
         textView1.setTypeface(typeface);
